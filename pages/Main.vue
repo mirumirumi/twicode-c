5c806e6a6ecf7c4304dc9d1d9d7db1dda0d12379
mirumi <info@milmemo.net>
2022-01-03 18:27:42 +0900

    [change] : Vue3 -> Nuxt3

---
+++ pages/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    {{ hello }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+const hello = ref("main")
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/Terms.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    {{ hello }}
+    <NuxtLink to="/">index</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+const hello = ref("terms")
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
