82f970fa1ffd2195e821f18a78427c1b75468d58
mirumi <info@milmemo.net>
2022-01-07 23:38:57 +0900

    [remove] : content of Footer.vue

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="footer-wrap">
+  <div class="footer_wrap">
+    <footer>
+
+    </footer>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
-
 </script>
 
 <style scoped lang="scss">
-.footer-wrap {
-}
 </style>
