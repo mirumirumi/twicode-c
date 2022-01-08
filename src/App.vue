5f5d0366b24124a55e211049a4c7d072941aa69a
mirumi <info@milmemo.net>
2022-01-08 11:39:52 +0900

    [add] : dynamic css height (quit onMoutend and ref)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
-  <Header></Header>
+  <TheHeader></TheHeader>
   <main class="view_wrap">
     <router-view/>
   </main>
-  <Footer></Footer>
+  <TheFooter></TheFooter>
 </template>
 
 <script setup lang="ts">
-import Header from "@/components/layouts/Header.vue"
-import Footer from "@/components/layouts/Footer.vue"
+// import { onMounted, ref } from "vue"
+import TheHeader from "@/components/layouts/TheHeader.vue"
+import TheFooter from "@/components/layouts/TheFooter.vue"
+
+// let headerHeight = ref<number | string | undefined>(0)
+// let footerHeight = ref<number | string | undefined>(0)
+
+// onMounted(() => {
+//   headerHeight.value = document.querySelector(".header_wrap")?.clientHeight.toString() + "px"
+//   footerHeight.value = document.querySelector(".footer_wrap")?.clientHeight.toString() + "px"
+// })
 </script>
 
 <style lang="scss">
@@ -21,5 +30,12 @@ import Footer from "@/components/layouts/Footer.vue"
   font-family: Avenir, Helvetica, Arial, sans-serif;
   width: 100vw;
   height: 100vh;
+}
+main.view_wrap {
+  margin: 0 13px;
+  width: auto;
+  max-width: calc(100% - 13px * 2);
+  // height: calc(100vh - v-bind(headerHeight) - v-bind(footerHeight));
+  height: calc(100vh - $header_height - $footer_height);
 }
 </style>
